5b6e9ce699686a6e237be4c7d921206622e0e0f5
BodySites <100404530+BodySites@users.noreply.github.com>
2022-11-18 18:15:08 +0600

    Slider display error fix
    Changing the assignment of a value to the active slide on page load.

---
+++ lab-2/script.js
@@ -0,0 +1,58 @@
+const slides = document.querySelectorAll('.slides>img')
+const left = document.querySelector('.button-left')
+const right = document.querySelector('.button-right')
+const countSlides = slides.length
+
+let activeSlideIndex
+
+let timeChange = 0
+
+window.addEventListener('DOMContentLoaded', () => {
+   activeSlideIndex = (parseInt(localStorage.getItem('activeSlide'))) ? parseInt(localStorage.getItem('activeSlide')) : 0
+   slides.forEach((slide, key) => {
+      slide.style.left = `calc(100vw*${key - activeSlideIndex})`
+   })
+})
+
+left.addEventListener('click', () => {
+   changeSlide('left')
+})
+right.addEventListener('click', () => {
+   changeSlide('right')
+})
+
+document.addEventListener('keyup', (event) => {
+   if (event.key === 'ArrowLeft') {
+      changeSlide('left')
+   }
+   else if (event.key === 'ArrowRight') {
+      changeSlide('right')
+   }
+   else if (event.key === 'Space') {
+      changeSlide('right')
+   }
+})
+
+function changeSlide(direction) {
+   timeChange = performance.now()
+   if (direction === 'left') {
+      if (--activeSlideIndex < 0) activeSlideIndex = countSlides - 1
+   }
+   else if (direction === 'right') {
+      if (++activeSlideIndex === countSlides) activeSlideIndex = 0
+   }
+
+   slides.forEach((slide, key) => {
+      slide.style.left = `calc(100vw*${key - activeSlideIndex})`
+   })
+}
+
+setInterval(() => {
+   if (performance.now() - timeChange >= 6000) {
+      changeSlide('right')
+   }
+}, 0)
+
+window.addEventListener('unload', () => {
+   localStorage.setItem('activeSlide', activeSlideIndex)
+})
