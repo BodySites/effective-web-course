87b39c67e31ead4ce268418142f0e112c5ea0319
BodySites <100404530+BodySites@users.noreply.github.com>
2022-12-17 23:43:08 +0600

    add routes and click handler for detail cards

--- react/src/Router.tsx
+++ react/src/Router.tsx
@@ -1,7 +1,10 @@
 import { RouteObject } from "react-router-dom";
+import CharacterPage from "./pages/CharacterPage";
 import Characters from "./pages/Characters";
+import ComicPage from "./pages/ComicPage";
 import Comics from "./pages/Comics";
 import NotFound from "./pages/NotFound";
+import SerialPage from "./pages/SerialPage";
 import Series from "./pages/Series";
 
 const router: RouteObject[] = [
@@ -10,12 +13,24 @@ const router: RouteObject[] = [
     element: <Characters />,
   },
   {
+    path: "/characters/:id",
+    element: <CharacterPage />,
+  },
+  {
     path: "/comics",
     element: <Comics />,
   },
   {
+    path: "/comics/:id",
+    element: <ComicPage />,
+  },
+  {
     path: "/series",
     element: <Series />,
+  },
+  {
+    path: "/series/:id",
+    element: <SerialPage />,
   },
   {
     path: "*",
--- react/src/components/Card.tsx
+++ react/src/components/Card.tsx
@@ -1,10 +1,18 @@
 import React, { FC } from "react";
 import cardType from "../types/cardType";
 import styles from "../styles/Components/Card.module.css";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Card: FC<cardType> = ({ id, name, img, description }) => {
+  const url: string = useLocation().pathname;
+  const navigate = useNavigate();
+
+  function Redirect() {
+    navigate(url + "/" + id.toString());
+  }
+
   return (
-    <div className={styles.card}>
+    <div className={styles.card} onClick={Redirect.bind(this)}>
       <img src={img} alt="" />
       <div className={styles.name}>{name}</div>
       <div className={styles.description}>{description}</div>
