d62fd73da55a3c3d10dddf7952e4014419600d88
BodySites <100404530+BodySites@users.noreply.github.com>
2022-12-23 00:05:41 +0600

    Update Footer.tsx

--- react/src/components/Footer.tsx
+++ react/src/components/Footer.tsx
@@ -1,10 +1,11 @@
 import stylesFooter from "../styles/Components/Footer.module.css";
+import marvelLogo from "../../public/marvel_logo.svg";
 
 export default function Footer() {
-  const year: number = new Date().getUTCFullYear();
+  const year = new Date().getUTCFullYear();
   return (
     <div className={stylesFooter.footer}>
-      <img src="public/marvel_logo.svg" alt="Marvel logo" />
+      <img src={marvelLogo} alt="Marvel logo" />
       <div>Data provided by Marvel. © {year} MARVEL</div>
       <a href="https://developer.marvel.com" target="_blank">
         developer.marvel.com
