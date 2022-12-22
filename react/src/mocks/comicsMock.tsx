94bdaf52fe78b1dac4124d05d44acbca4045c9ff
BodySites <100404530+BodySites@users.noreply.github.com>
2022-12-23 00:17:22 +0600

    Update comicsMock.tsx

--- react/src/mocks/comicsMock.tsx
+++ react/src/mocks/comicsMock.tsx
@@ -1,32 +1,96 @@
-import cardType from "../types/cardType";
+import { comicType } from "../types/comicType";
 
-const comicsMock: cardType[] = [
+const comicsMock: comicType[] = [
   {
     id: 30,
     name: "Marvel Tales",
-    img: "src/assets/comics/clean.jpg",
+    img: "../src/assets/comics/clean.jpg",
     description:
       "Four of the cornerstone issues that ushered in the Marvel Age of Comics in one unbeatable package",
+    characters: [
+      {
+        id: 10,
+        name: "Abyss",
+        img: "../src/assets/heroes/2_3.jpg",
+        description:
+          "Mutant, the owner of unique (even by the standards of mutants) abilities",
+      },
+      {
+        id: 11,
+        name: "3D-Man",
+        img: "../src/assets/heroes/146474-83827-3-d-man.jpg",
+        description:
+          "The three-dimensional man was a hero of the 1950s, who appeared due to the merger of two brothers, Hal and Chuck Chandler, and the subsequent merger with Delroy Garrett Jr",
+      },
+    ],
   },
   {
     id: 31,
     name: "Ultimate comics spider-man 1",
-    img: "src/assets/comics/clean2.jpg",
+    img: "../src/assets/comics/clean2.jpg",
     description:
       "Meet a brand-new Spider-Man for the 21st century all over again",
+    characters: [
+      {
+        id: 10,
+        name: "Abyss",
+        img: "../src/assets/heroes/2_3.jpg",
+        description:
+          "Mutant, the owner of unique (even by the standards of mutants) abilities",
+      },
+      {
+        id: 11,
+        name: "3D-Man",
+        img: "../src/assets/heroes/146474-83827-3-d-man.jpg",
+        description:
+          "The three-dimensional man was a hero of the 1950s, who appeared due to the merger of two brothers, Hal and Chuck Chandler, and the subsequent merger with Delroy Garrett Jr",
+      },
+    ],
   },
   {
     id: 32,
     name: "Dark Web: X-Men",
-    img: "src/assets/comics/clean3.jpg",
+    img: "../src/assets/comics/clean3.jpg",
     description:
       "The X-Men are ensnared in Chasm and The Goblin Queen's Dark Web",
+    characters: [
+      {
+        id: 10,
+        name: "Abyss",
+        img: "../src/assets/heroes/2_3.jpg",
+        description:
+          "Mutant, the owner of unique (even by the standards of mutants) abilities",
+      },
+      {
+        id: 11,
+        name: "3D-Man",
+        img: "../src/assets/heroes/146474-83827-3-d-man.jpg",
+        description:
+          "The three-dimensional man was a hero of the 1950s, who appeared due to the merger of two brothers, Hal and Chuck Chandler, and the subsequent merger with Delroy Garrett Jr",
+      },
+    ],
   },
   {
     id: 33,
     name: "Star Wars: Bounty Hunters",
-    img: "src/assets/comics/clean4.jpg",
+    img: "../src/assets/comics/clean4.jpg",
     description: "BOSSK VS. TANKA!",
+    characters: [
+      {
+        id: 10,
+        name: "Abyss",
+        img: "../src/assets/heroes/2_3.jpg",
+        description:
+          "Mutant, the owner of unique (even by the standards of mutants) abilities",
+      },
+      {
+        id: 11,
+        name: "3D-Man",
+        img: "../src/assets/heroes/146474-83827-3-d-man.jpg",
+        description:
+          "The three-dimensional man was a hero of the 1950s, who appeared due to the merger of two brothers, Hal and Chuck Chandler, and the subsequent merger with Delroy Garrett Jr",
+      },
+    ],
   },
 ];
 
